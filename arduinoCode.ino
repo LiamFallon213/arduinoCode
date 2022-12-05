7cb0c9483416271d38872f5832686bbc270ca099
LiamFallon213 <81399949+LiamFallon213@users.noreply.github.com>
2022-12-05 09:44:21 -0700

    a

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,152 +1,167 @@
 #include <TimeLib.h>
 #include <Dictionary.h>
 #include <SoftwareSerial.h>
+#include <AFMotor.h>
+#include <Servo.h> 
 #define _DICT_CRC_16
 
-// globals
-SoftwareSerial BTSerial(10, 11); // RX | TX
-//String JSON = "{\"108\":\"left\",\"114\": \"right\",\"117\": \"up\",\"100\": \"down\"}"
-Dictionary& controlMap = *(new Dictionary());
+#include <AFMotor.h>
+AF_DCMotor leftMotorOBJ(4, MOTOR12_64KHZ);
+AF_DCMotor rightMotorOBJ(3, MOTOR12_64KHZ);
 
+// globals
+SoftwareSerial BTSerial(11, 10); // RX | TX
+//String JSON = "{\"108\":\"left\",\"114\": \"right\",\"117\": \"up\",\"100\": \"down\",\"85\": \"servoUp\",\"68\": \"servoDown\"}"
+Dictionary& controlMap = *(new Dictionary());
 
 
 void writeEngine(int input);
 void writeServo(int input);
 
-class Command {
-  private:
-    int portNumber;
-    int value;
-    int type;
-    time_t timeStamp;
 
+class Engine {
   public:
-
-    Command(int portNumberIn, int valueIn, String commandTypeIn) {
-      portNumberIn = portNumber;
-      valueIn = value;
-      commandTypeIn = type;
-      timeStamp = now();
+    int spd;
+    const static int staticTurningSpeed = 200;
+    int turnSpeed;
+    String dir;
+    AF_DCMotor* motorPointer;
+    Engine(AF_DCMotor* motorIn) {
+      motorPointer = motorIn;
+      spd = 0;
+      
+      turnSpeed = 0;
+      dir = "FOWARD";
     }
 
-    int getPN() {
-      return portNumber;
-    };
-
-    void setPN(int input) {
-      portNumber = input;
-    };
-
-    int getValue() {
-      return value;
-    };
-
-    void setValue(int input) {
-      value = input;
-    };
-
-    time_t getTimeStamp() {
-      return timeStamp;
-    };
-
-    int getType() {
-      return type;
-    };
-
-    void setType(int input) {
-      type = input;
-    };
-};
-
-class Component {
-  private:
-    int value;
-    int pin;
-    bool active;
-
-  public:
-    int getValue() {
-      return value;
-    };
-    int setValue(int val) {
-      value = val;
-    };
-    int getPin() {
-      return pin;
-    };
-
-    void setPin(int val) {
-      pin = val;
-    };
-
-    // Takes in command, which is a pointer to a Command object
-    void execute(Command* command) {
-      switch (command->getType()) {
-
-        case 1: writeEngine(command->getValue());//engine speed command
-          break;
-
-        case 2: writeServo(command->getType()); // Servo angle control
-          break;
-
+    _update() {
+      int tempSpd = spd + turnSpeed;
+      if (tempSpd > 0) {
+        motorPointer->run(FORWARD);
+      } else if (tempSpd == 0) {
+        motorPointer->run(RELEASE);
+      } else if (tempSpd < 0) {
+        motorPointer->run(BACKWARD);
       }
-    };
+      motorPointer->setSpeed(abs(tempSpd));
+      //    Serial.println(spd);
+      Serial.println(abs(spd));
+    }
 };
 
-class Engine: public Component {
+Engine leftMotor = Engine(&leftMotorOBJ);
+
+Engine rightMotor = Engine(&rightMotorOBJ);
+
+class ServoClass {
   public:
     int angle;
-    int enginePosition;
 };
 
-class ServoClass: public Component {
-  public:
-    int angle;
-};
-
-void handleEngines();
-
-Command makeCommand(String input) {
+void left();
+void right();
+void up();
+void down();
+void _stop();
+void servoUp();
+void servoDown();
+Servo myservo;
+void makeCommand(String input) {
   String dir = controlMap[input];
   int inputInt = input.toInt();
   if (dir == "left") {
-    handleEngines();
+    left();
   } else if (dir == "right") {
-    handleEngines();
+    right();
   } else if (dir == "up") {
-    handleEngines();
+    up();
   } else if (dir == "down") {
-    handleEngines();
+    down();
+  } else if (dir == "stop") {
+    _stop();
+  } else if (dir == "servoUp") {
+   myservo.write(150);
+  } else if (dir == "servoDown") {
+   myservo.write(60);
   }
-  Serial.println(dir);
 }
 
-void handleEngines(){
-  Serial.println("Handle Engines");
+void servoUp() {
+
 }
 
-float phSensor(){
- int analogIn = analogRead(0);
- float voltage = ((float)analogIn / 1023)*5;
- float PH = (-3/0.52)*voltage+21.47;
- return voltage;
+void left() {
+  //  Serial.println("left()");
+  leftMotor.spd = 0;
+  rightMotor.spd = 0;
+  leftMotor.turnSpeed = (-leftMotor.staticTurningSpeed);
+  rightMotor.turnSpeed = rightMotor.staticTurningSpeed;
 }
+
+void right() {
+  //Serial.println("right()");
+  leftMotor.spd = 0;
+  rightMotor.spd = 0;
+  leftMotor.turnSpeed = leftMotor.staticTurningSpeed;
+  rightMotor.turnSpeed = (-rightMotor.staticTurningSpeed);
+}
+
+void up() {
+  leftMotor.turnSpeed = 0;
+  rightMotor.turnSpeed = 0;
+  leftMotor.spd += 70;
+  rightMotor.spd += 70;
+  if (leftMotor.spd > 255) leftMotor.spd = 255;
+  if (rightMotor.spd > 255) rightMotor.spd = 255;
+}
+
+void down() {
+  // Serial.println("down()");
+    leftMotor.turnSpeed = 0;
+  rightMotor.turnSpeed = 0;
+  leftMotor.spd -= 70;
+  rightMotor.spd -= 70;
+  if (leftMotor.spd < -255) leftMotor.spd = -255;
+  if (rightMotor.spd < -255) rightMotor.spd = -255;
+}
+
+void _stop() {
+  leftMotor.spd = 0;
+  rightMotor.spd = 0;
+  leftMotor.turnSpeed = 0;
+  rightMotor.turnSpeed = 0;
+};
+
+void _update_() {
+  leftMotor._update();
+  rightMotor._update();
+};
+
+float phSensor() {
+  int analogIn = analogRead(0);
+  float voltage = ((float)analogIn / 1023) * 5;
+  float PH = (-3 / 0.52) * voltage + 22.47;
+  return PH;
+};
 
 
 
 void setup() {
+  
   Serial.begin(9600);
   BTSerial.begin(9600);
-  controlMap.jload("{\"108\":\"left\",\"114\": \"right\",\"117\": \"up\",\"100\": \"down\"}");
+  controlMap.jload("{\"108\":\"left\",\"114\": \"right\",\"117\": \"up\",\"100\": \"down\",\"85\": \"servoUp\",\"68\": \"servoDown\"}");
   //  Serial.println(controlMap.json());
+  myservo.attach(10); 
+  myservo.write(60);
 };
 
 void loop() {
-  Serial.println(phSensor());
+  _update_();
+  BTSerial.println((float)phSensor());
   if (BTSerial.available()) { // read from HC-05 and send to Arduino Serial Monitor
     String BTInput;
     BTInput += BTSerial.read();
-  //  Serial.println(BTInput);
     makeCommand(BTInput);
   }
 
